e291c28839066cb809b972cbe035efb7edc4aed8
renatoxp51 <reenato.sl09@gmail.com>
2023-07-14 14:38:09 -0300

    Implementando endpoint para mudar a posição dos games na lista

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -16,6 +18,9 @@ public class GameListService {
 	
 	@Autowired
 	private GameListRepository gameListRepository;
+	
+	@Autowired
+	private GameRepository gameRepository;
 	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
@@ -25,6 +30,26 @@ public class GameListService {
 		/* faz com que cada objeto da lista original (resultado) seja um GameListDTO */
 		List<GameListDTO> dto = resultado.stream().map(x -> new GameListDTO(x)).toList();
 		return dto;
+		
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		
+		List<GameMinProjection> lista = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = lista.remove(sourceIndex);
+		
+		lista.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for (int i = min; i<= max; i++) {
+			
+			gameListRepository.updateBelongingPosition(listId, lista.get(i).getId(), i);
+			
+		}
 		
 	}
 		
